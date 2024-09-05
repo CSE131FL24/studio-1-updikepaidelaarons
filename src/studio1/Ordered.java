2e66de8b3893129370c22a83e151857b4aebba0c
emily <emily@10.232.188.113>
2024-09-05 12:20:34 -0500

    done!

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,6 +15,20 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+		
+		boolean isOrdered = true;
+		
+		if ((x>y && y>z) || (x<y && y<z)) {
+			System.out.print(isOrdered);
+		}
+		
+		else {
+			System.out.print(false);
+			
+		
+		}
+		
+		
 
 	}
 
